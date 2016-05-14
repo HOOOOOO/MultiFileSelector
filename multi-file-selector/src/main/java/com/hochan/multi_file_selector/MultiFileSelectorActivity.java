d6430b7a5f4ba0634d8f4641533dbefd9628a938
HOOOOOO <1227889585@qq.com>
2016-05-14 09:20:03 +0800

    First Commit

---
+++ multi-file-selector/src/main/java/com/hochan/multi_file_selector/MultiFileSelectorActivity.java
@@ -0,0 +1,13 @@
+package com.hochan.multi_file_selector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiFileSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_file_selector);
+    }
+}
